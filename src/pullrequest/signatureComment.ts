e2dc261b87c7ebbbd9efb57d6de0ab15b11d69c8
Tomer Greenwald <tomerg@gmail.com>
2022-10-10 11:59:03 +0300

    Updated user name and email

--- src/pullrequest/signatureComment.ts
+++ src/pullrequest/signatureComment.ts
@@ -16,11 +16,16 @@ export default async function signatureWithPRComment(committerMap: CommitterMap,
     let listOfPRComments = [] as CommittersDetails[]
     let filteredListOfPRComments = [] as CommittersDetails[]
 
+    let hashedCommitters = committers.reduce(function(map, obj) {
+        map[obj.name] = obj;
+        return map;
+    }, {});
+
     prResponse?.data.map((prComment) => {
         listOfPRComments.push({
             name: prComment.user.login,
-            userName: prComment.user.login,
-            userEmail: prComment.user.login,
+            userName: hashedCommitters[name].userName,
+            userEmail: hashedCommitters[name].userEmail,
             id: prComment.user.id,
             comment_id: prComment.id,
             body: prComment.body.trim().toLowerCase(),
